7b0c374cb63337e1296096153900fbf42e7ec03d
Chen (Thomas) Yang <chenyangfinance@gmail.com>
2026-02-22 17:21:36 +0000

    Modify navigation links in layout.js
    Updated navigation links to change 'Home' to 'About' and added 'Blog' link.

--- app/layout.js
+++ app/layout.js
@@ -37,13 +37,13 @@ export default function RootLayout({ children }) {
               Chen Yang
             </Link>
             
-            <nav style={{ display: 'flex', gap: '25px', alignItems: 'center' }}>
-              <Link href="/" style={navLinkStyle}>Blog</Link>
-              <Link href="/about" style={navLinkStyle}>About</Link>
-              <Link href="/research" style={navLinkStyle}>Research</Link>
-              <Link href="/teaching" style={navLinkStyle}>Teaching</Link>
-              <Link href="/cnabout" style={{ ...navLinkStyle, color: '#0066cc' }}>中文</Link>
-            </nav>
+        <nav style={{ display: 'flex', gap: '25px', alignItems: 'center' }}>
+          <Link href="/" style={navLinkStyle}>About</Link> {/* 首页改为 About */}
+          <Link href="/blog" style={navLinkStyle}>Blog</Link> {/* 增加 Blog 链接 */}
+          <Link href="/research" style={navLinkStyle}>Research</Link>
+          <Link href="/teaching" style={navLinkStyle}>Teaching</Link>
+          <Link href="/cnabout" style={{ ...navLinkStyle, color: '#0066cc' }}>中文</Link>
+        </nav>
           </div>
         </header>
 
